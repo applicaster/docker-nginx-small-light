26c540ae891ad44d6bbb6995215c5918bc5db8e4
Vitaly Gorodetsky <gorodetsky@gmail.com>
2017-12-26 11:48:57 +0200

    fix

--- .circleci/setup_terraform.sh
+++ .circleci/setup_terraform.sh
@@ -1,3 +1,3 @@
 which asdf || git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 (asdf plugin-list | grep terraform) || asdf plugin-add terraform https://github.com/neerfri/asdf-terraform.git
-asdf install
+cd terraform && asdf install
